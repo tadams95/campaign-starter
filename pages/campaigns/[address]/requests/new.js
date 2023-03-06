5b634af5b3efbe941a9532fb3b0838a8a8ef8939
tadams95 <tyrelleadams28@gmail.com>
2023-03-06 15:22:51 -0800

    Fix RequestNew page

--- pages/campaigns/[address]/requests/new.js
+++ pages/campaigns/[address]/requests/new.js
@@ -1,55 +1,58 @@
-import React, { Component, useState } from "react";
-import Layout from "@/components/Layout";
-import { Form, Button, Input, Message } from "semantic-ui-react";
-import factory from "@/ethereum/factory";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { Form, Button, Message, Input } from "semantic-ui-react";
+import Campaign from "@/ethereum/campaign";
 import web3 from "@/ethereum/web3";
-import { useRouter } from 'next/router';
+import Layout from "@/components/Layout";
 
-const CampaignNew = () => {
-  const [minimumContribution, setMinimumContribution] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [loading, setLoading] = useState(false);
-
+const RequestNew = () => {
   const router = useRouter();
+  const { address } = router.query;
+
+  const [value, setValue] = useState("");
+  const [description, setDescription] = useState("");
+  const [recipient, setRecipient] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
+    const campaign = Campaign(address);
+
     setLoading(true);
+    setErrorMessage("");
 
     try {
       const accounts = await web3.eth.getAccounts();
-      //create a new campaign
-      await factory.methods
-        .createCampaign(minimumContribution)
-        .send({
-          from: accounts[0],
-        });
-
-      router.push("/");
-    } catch (error) {
-      setErrorMessage(error.message);
+      await campaign.methods
+        .createRequest(description, web3.utils.toWei(value, "ether"), recipient)
+        .send({ from: accounts[0] });
+      router.push(`/campaigns/${address}/requests`);
+    } catch (err) {
+      setErrorMessage(err.message);
     }
-
     setLoading(false);
   };
 
   return (
     <Layout>
-      <h3>Create a campaign</h3>
+      <a href={`/campaigns/${address}/requests`}>Back</a>
+      <h3>Create a Request</h3>
       <Form onSubmit={onSubmit} error={!!errorMessage}>
         <Form.Field>
-          <label>Minimum Contribution</label>
-          <Input
-            label="Wei"
-            labelPosition="right"
-            value={minimumContribution}
-            onChange={(event) =>
-              setMinimumContribution(event.target.value)
-            }
-          />
+          <label>Description</label>
+          <Input value={description} onChange={(e) => setDescription(e.target.value)} />
         </Form.Field>
-        <Message error header="Uh-Oh!" content={errorMessage} />
+        <Form.Field>
+          <label>Value in Ether</label>
+          <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        </Form.Field>
+        <Form.Field>
+          <label>Recipient</label>
+          <Input value={recipient} onChange={(e) => setRecipient(e.target.value)} />
+        </Form.Field>
+        <Message error header="Oops!" content={errorMessage} />
         <Button primary loading={loading}>
           Create!
         </Button>
@@ -58,4 +61,4 @@ const CampaignNew = () => {
   );
 };
 
-export default CampaignNew;
+export default RequestNew;
